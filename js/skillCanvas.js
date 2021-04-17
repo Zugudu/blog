2035fbfee0b852ab54e5905d6f1d040865c0de68
Zugudu <2cuco2@gmail.com>
2021-04-18 02:23:40 +0300

    style

--- js/skillCanvas.js
+++ js/skillCanvas.js
@@ -7,9 +7,9 @@ let iKnown = [
 	[], //2015
 	['Java'], //2016
 	['Linux', 'SQL'], //2017
-	['Assembler', 'AVR', 'HTML/CSS'], //2018
+	['Assembler', 'AVR', 'HTML/CSS', 'PHP'], //2018
 	['Pure C', 'Docker'], //2019
-	['Python', 'Bottle', 'Flask', 'Django'], //2020
+	['Python', 'Bottle', 'Flask', 'Django', 'Bootstrap'], //2020
 	['NodeJS', 'MongoDB', 'AngularJS', 'K8S', 'Ansible'], //2021
 ]
 let dx = document.getElementById('history').width/(iKnown.length+1);
